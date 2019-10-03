1de42c9f26534579dbe827d51979fed8ab84cf80
Mohammed Almalki <m7mdmallki@gmail.com>
2019-10-03 11:37:57 +0300

    Cars and Cat Done

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
 const http = require('http');
 const fs = require('fs');
-const server = http.createServer(function (request, response){
+const server = http.createServer(function (request, response) {
     console.log('client request URL: ', request.url);
-    
+
     // this is how we do routing:
-    if(request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            response.write("Hello from Node");  //  send response body
-            response.end(); // finished!
-        });
-    }
-    else if(request.url === '/ninjas') {
-        fs.readFile('ninjas.html', 'utf8', function (errors){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
-            response.write("info about ninja");  //  send response body
-            response.end(); // finished!
-        });
-    }
-    else if(request.url === '/ninjas/new') {
-        fs.readFile('dojo.html', 'utf8', function (errors,Content){
-            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+    if (request.url === '/cars') {
+        fs.readFile('./views/index.html', 'utf8', function (errors, Content) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });  // send data about response
             response.write(Content);  //  send response body
             response.end(); // finished!
         });
     }
+    else if (request.url === '/cats') {
+        fs.readFile('./views/cats.html', 'utf8', function (errors, Content) {
+            response.writeHead(200, { 'Content-Type': 'text/html' });  // send data about response
+            response.write(Content);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    else if (request.url === '/images/cls.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cls.jpg', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'image/jpg' });
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/m5.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/m5.jpg', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'image/jpg' });
+            response.write(contents);
+            response.end();
+        })
+    }
 
-    
+
+    else if (request.url === '/images/cat.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cat.jpg', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'image/jpg' });
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/images/pizza.jpg') {
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/pizza.jpg', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'image/jpg' });
+            response.write(contents);
+            response.end();
+        })
+    }
+    else if (request.url === '/stylesheets/style.css') {
+        fs.readFile('./stylesheets/style.css', 'utf8', function (errors, contents) {
+            response.writeHead(200, { 'Content-type': 'text/css' });
+            response.write(contents);
+            response.end();
+        })
+    }
+
+
+
+
     // request didn't match anything:
     else {
         response.writeHead(404);
